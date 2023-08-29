08313c3c8c9cb77b886f5034c921de47708e101f
Patrick <patrick.s.mason@gmail.com>
2023-08-29 09:57:23 -0600

    NXDOMAIN support

--- resolver.py
+++ resolver.py
@@ -9,6 +9,7 @@
 """
 
 import dnslib
+import enum
 import socket
 import random
 import time
@@ -30,30 +31,36 @@ def get_random_root_server():
 
 
 # Plain-old-data type for respones from name servers.
-class NameServerResponse(typing.NamedTuple):
+class NameServerResponseType(enum.Enum):
+    # An authoritative server returned NXDOMAIN.
+    NXDOMAIN = 0
+
     # IPv4 IP address
-    answer: str = ""
+    ANSWER = 1
 
     # The response provided both a delegate authority server, and its 
     # IPv4 address
-    delegate_ip_address: str = ""
+    DELEGATE_IP_ADDRESS = 2
 
     # The response provided only the name of a delegated authority server.
     # IPv4 addresses need to be queries as followup work.
-    delegate_name: str = ""
+    DELEGATE_NAME = 3
 
     # A CNAME answer that requires chasing.
-    cname: str = ""
+    CNAME = 4
 
 
-def get_address_from_response(parsed_response):
+def structure_response(parsed_response):
+    if parsed_response.header.rcode == dnslib.RCODE.NXDOMAIN:
+        return "", NameServerResponseType.NXDOMAIN
+
     if len(parsed_response.rr):
         # An answer is available
         answer = parsed_response.get_a()
         if answer.rtype == dnslib.QTYPE.A:
-            return NameServerResponse(answer=str(answer.rdata))
+            return str(answer.rdata), NameServerResponseType.ANSWER
         elif answer.rtype == dnslib.QTYPE.CNAME:
-            return NameServerResponse(cname=str(answer.rdata))
+            return str(answer.rdata), NameServerResponseType.CNAME
     
     # If there no answer, pick from the authority section for the recursive, followup query.
     auth = random.choice(parsed_response.auth)
@@ -64,10 +71,10 @@ def get_address_from_response(parsed_response):
         if ar.rtype == dnslib.QTYPE.A and ar.rname == str(auth.rdata):
             delegate_ip_address = str(ar.rdata)
     if delegate_ip_address is not None:
-        return NameServerResponse(delegate_ip_address=delegate_ip_address)
+        return delegate_ip_address, NameServerResponseType.DELEGATE_IP_ADDRESS
     
     print("Hit a dead end. Scheduling a new root query:", str(auth.rdata))
-    return NameServerResponse(delegate_name=str(auth.rdata))
+    return str(auth.rdata), NameServerResponseType.DELEGATE_NAME
 
 
 def query_name_server(qname, name_server_ip_address):
@@ -81,7 +88,7 @@ def query_name_server(qname, name_server_ip_address):
     client_socket.bind(('', 0))        
     
     # In case of bugs, let's make sure we query nameservers slowly.
-    time.sleep(1.0)
+    time.sleep(0.5)
 
     print("querying name server for qname:", qname, (name_server_ip_address, 53))
     client_socket.sendto(query.pack(), (name_server_ip_address, 53))
@@ -90,7 +97,7 @@ def query_name_server(qname, name_server_ip_address):
     parsed_response = dnslib.DNSRecord.parse(raw_response)
     print("received name server response:", parsed_response)
 
-    return get_address_from_response(parsed_response)
+    return structure_response(parsed_response)
 
 
 def resolve_name(question):
@@ -112,36 +119,42 @@ def resolve_name(question):
             dnslib.DNSHeader(qr=1,aa=0,ra=1), 
             q=question)
     while True:
-        name_server_response = query_name_server(query_name_stack[-1], current_name_server_address)
+        name_server_response, name_server_response_type = query_name_server(query_name_stack[-1], current_name_server_address)
         
-        if name_server_response.answer:
+        if name_server_response_type == NameServerResponseType.ANSWER:
             # We've answered whatever work item is at the top of the stack.
             query_name_stack.pop()
             if len(query_name_stack) == 0:
                 # We're done. 
-                response.rr.append(dnslib.RR(str(question.get_qname()), rdata=dnslib.A(name_server_response.answer)))
+                response.rr.append(dnslib.RR(str(question.get_qname()), rdata=dnslib.A(name_server_response)))
                 return response
             else:
                 # Keep working, using the answer as the next name server address
-                current_name_server_address = name_server_response.answer
-        elif name_server_response.delegate_name:
+                current_name_server_address = name_server_response
+        elif name_server_response_type == NameServerResponseType.DELEGATE_NAME:
             # We're still trying to answer some targeted resource name, so it stays 
             # in the work stack. But there's an additional name server to resolve, 
             # which needs to be resolved from the root.
-            query_name_stack.append(name_server_response.delegate_name)
+            query_name_stack.append(name_server_response)
             # Updating ns address to restart resolution from a root server
             current_name_server_address = get_random_root_server()
-        elif name_server_response.cname:
+        elif name_server_response_type == NameServerResponseType.CNAME:
             # CNAME records need to be reported to the client.
-            response.rr.append(dnslib.RR(query_name_stack[-1], rtype=dnslib.QTYPE.CNAME, rdata=dnslib.CNAME(name_server_response.cname)))
+            response.rr.append(dnslib.RR(query_name_stack[-1], rtype=dnslib.QTYPE.CNAME, rdata=dnslib.CNAME(name_server_response)))
             # The CNAME result means there's no further work to do for the current resource name.
             query_name_stack.pop()
             # But we chase down the CNAME, starting from the root. 
-            query_name_stack.append(name_server_response.cname)
+            query_name_stack.append(name_server_response)
             current_name_server_address = get_random_root_server()
-        else:
+        elif name_server_response_type == NameServerResponseType.DELEGATE_IP_ADDRESS:
             # Keep traversing with the updating name server address
-            current_name_server_address = name_server_response.delegate_ip_address
+            current_name_server_address = name_server_response
+        elif name_server_response_type == NameServerResponseType.NXDOMAIN:
+            # NXDOMAIN interrupts all work. 
+            response.header.set_rcode(dnslib.RCODE.NXDOMAIN)
+            return response
+        else:
+            raise Exception("Unexpected response type:", name_server_response_type)
 
 
 def deliver_response(socket, response_bytes, address):
